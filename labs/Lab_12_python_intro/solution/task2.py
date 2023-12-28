cf0aa45e31262637a93dce131f0d5c0eeb2a16a8
Anton <antonshev03@gmail.com>
2023-12-28 13:51:47 +0300

    laba 12,13

--- labs/Lab_12_python_intro/solution/task2.py
+++ labs/Lab_12_python_intro/solution/task2.py
@@ -1,23 +1,14 @@
-def median_sum(n, X):
-    medians = []
-
-    for i in range(1, n + 1):
-        subsequence = X[:i]
-        subsequence.sort()
-
-        length = len(subsequence)
-        if length % 2 == 1:
-            median = subsequence[length // 2]
-        else:
-            median = (subsequence[length // 2 - 1] + subsequence[length // 2]) / 2
-
-        medians.append(median)
-
-    median_sum = sum(medians)
-    return median_sum
-
-n = int(input("Введите n: "))
-X = list(map(int, input("Введите последовательность чисел X: ").split()))
-
-result = median_sum(n, X)
-print(result)
+import heapq
+def find(n, arr):
+    max = []
+    min = []
+    result = 0
+    for i in range(n):
+        heapq.heappush(max, -arr[i])
+        heapq.heappush(min, -heapq.heappop(max))
+        if len(min) > len(max):
+            heapq.heappush(max, -heapq.heappop(min))
+        result -= max[0]
+    return result
+n, arr = int(input()), list(map(int, input().split()))
+print(find(n, arr))
--- labs/Lab_12_python_intro/solution/task3.py
+++ labs/Lab_12_python_intro/solution/task3.py
@@ -1,18 +1,17 @@
-def histogram(text):
-    char_count = {}
-    for char in text:
-        if char != ' ' and char != '\n':
-            char_count[char] = char_count.get(char, 0) + 1
-
-    sorted_chars = sorted(char_count.keys())
-
-    max_count = max(char_count.values())
-
-    for char in sorted_chars:
-        count = char_count[char]
-        histogram_line = '#' * count
-        histogram_line += ' ' * (max_count - count)
-        print(histogram_line + ' ' + char)
-
-encrypted_text = input("Введите зашифрованный текст: ")
-histogram(encrypted_text)
+text = input("Input text: ")
+histo_dict = {}
+for i in text:
+    if i != " ":
+        if i in histo_dict:
+            histo_dict[i] = histo_dict[i] + 1
+        else:
+            histo_dict[i] = 1
+sorted_dict = dict(sorted(histo_dict.items()))
+for i in range(0, max(sorted_dict.values())):
+    for j in sorted_dict:
+        if sorted_dict[j] >= (max(sorted_dict.values()) - i):
+            print("#", end="")
+        else:
+            print(" ", end="")
+    print("\n", end="")
+print("".join(list(sorted_dict.keys())))
